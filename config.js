c95ab7d47d1b52c0960dc021e567184e61c4e06a
Tharuka Randima <121140826+Xtharuka@users.noreply.github.com>
2023-09-26 22:19:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,18 +10,18 @@ global.antitags = true;
 
 
 //auto functioner
-global.autoTyping = false;                //auto tying by default off.
+global.autoTyping = true;                //auto tying by default off.
 global.autoRecord = false;                //auto recording by default off.
 global.groupevent = false;                //This is the new variable for controlling group event handling.
 global.statusseen = true;                 //This is the new variable for controlling status seen.
 
 
 //
-global.Owner = ["8801853262586","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["8801853262586","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.Owner = ['94723357728'];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.OwnerNumber = ['94723357728'];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
 global.ownertag = ["8801928502256"];
-global.OwnerName = "Slasher";
-global.BotName = "XLICON-MD";
+global.OwnerName = "tharuka@virus.com";
+global.BotName = "XTHARUKA-MD";
 global.packname = "X-2.0 Bot";                             //Do not change.
 global.author = "By: Slasher";                               //Do not change.
 global.BotSourceCode = "https://github.com/ahil15/Xlicon-v2"; //Do not change.
